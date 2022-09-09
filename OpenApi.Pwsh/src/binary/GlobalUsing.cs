4edc9f395ab124cda8a0dc2347948a54a7efb9ff
Matt Gucci <matt9ucci@gmail.com>
2022-09-09 18:26:50 +0900

    Add global using: Microsoft.OpenApi, etc.

--- OpenApi.Pwsh/src/binary/GlobalUsing.cs
+++ OpenApi.Pwsh/src/binary/GlobalUsing.cs
@@ -1,3 +1,7 @@
 global using System.Collections;
 global using System.Management.Automation;
+global using Microsoft.OpenApi;
+global using Microsoft.OpenApi.Interfaces;
 global using Microsoft.OpenApi.Models;
+global using Microsoft.OpenApi.Readers;
+global using Microsoft.OpenApi.Writers;
